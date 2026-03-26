cc1570ff5fc39e5d0fb2ffc9f7e2861b4a8593f2
Yeison Moran <moranyeison522@gmail.com>
2026-03-26 18:49:43 -0500

    Mi primer portafolio

---
+++ assets/js/fuction.js
@@ -0,0 +1,37 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Desarrollador Web Junior', 'Soporte IT Especializado', 'Especialista en Mantenimiento'], 
+    typeSpeed: 70, 
+    backSpeed: 70, 
+    backDelay: 1000, 
+    loop: true 
+});
